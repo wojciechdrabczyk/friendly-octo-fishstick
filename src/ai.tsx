3055bc95d6333f5016cea69febc365dd5a0b24ba
wojciechdrabczyk <wojciechdrabczyk@icloud.com>
2025-04-14 19:49:35 +0200

    changed accessing the token in .env to vite compatible one

--- src/ai.tsx
+++ src/ai.tsx
@@ -3,7 +3,7 @@ import { HfInference } from '@huggingface/inference'
 const SYSTEM_PROMPT = `
 You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page
 `
-const hf = new HfInference(process.env.HF_ACCESS_TOKEN)
+const hf = new HfInference(import.meta.env.VITE_HF_ACCESS_TOKEN)
 
 export async function getRecipeFromMistral(ingredientsArr) {
     const ingredientsString = ingredientsArr.join(", ")
